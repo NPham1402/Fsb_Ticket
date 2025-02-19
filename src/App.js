270091fefe96f7ccfb2d897fb907ce8a62e7ccea
nguyen pham <npham140201@gmail.com>
2025-02-19 14:39:50 +0700

    update 19/2/2025

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <img
+        alt="logo"
+        src="https://fsb.edu.vn/wp-content/uploads/2023/06/logo.svg"
+        style={{ width: "300px", marginTop: "100px" }}
+      />
+      <h2>Web mượn/ trả tài sản của hành chính FSB Hồ Chí Minh</h2>
+      <p style={{ fontSize: "20px" }}>Bạn muốn mượn hay trả tài sản?</p>
+      <div>
+        <button
+          onClick={() => {
+            window.location.href = "https://forms.gle/8tRMZgHRAFxhgQHb6";
+          }}
+          style={{
+            width: 150,
+            height: 150,
+            border: 1,
+            borderRadius: 12,
+            backgroundColor: "#0064b4 ",
+            color: "white",
+            fontSize: "20px",
+            marginRight: "20px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Mượn tài sản
+        </button>
+        <button
+          onClick={() => {
+            window.location.href = "https://forms.gle/GAVygLb9rfeqRv8Q8";
+          }}
+          style={{
+            width: 150,
+            height: 150,
+            border: 1,
+            borderRadius: 12,
+            backgroundColor: "#0fb44b",
+            color: "white",
+            fontSize: "20px",
+          }}
+        >
+          Trả tài sản
+        </button>
+      </div>
     </div>
   );
 }
